0751899ee77283fda7acee525879b9075b0bd1c6
Chito Wong <chi.to.wong@outlook.com>
2022-03-03 00:58:39 +0800

    Setup redux provider and theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRoot from './components/app-root';
+import chakraTheme from './theme/theme';
+import store from './redux';
 
 const renderApp = () => {
     ReactDOM.render(
-        <React.StrictMode>
-            <AppRoot />
-        </React.StrictMode>,
+        <Provider store={store}>
+            <ChakraProvider theme={chakraTheme}>
+                <StrictMode>
+                    <AppRoot />
+                </StrictMode>
+            </ChakraProvider>
+        </Provider>,
         document.getElementById('root')
     );
 };
