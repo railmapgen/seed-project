b74f06f240dac2ac9586025bbc1aaa908497fefe
Chito Wong <chi.to.wong@outlook.com>
2022-04-27 23:35:59 +0800

    Update dependencies

--- src/components/window-header.tsx
+++ src/components/window-header.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import { Badge, Flex, Heading } from '@chakra-ui/react';
-import { Environments, getEnvironment, getVersion } from '../util/config';
+import { Flex, Heading } from '@chakra-ui/react';
+import { getEnvironment, getVersion } from '../util/config';
 import { useTranslation } from 'react-i18next';
+import { RmgEnvBadge } from '@railmapgen/rmg-components';
 
 export default function WindowHeader() {
     const { t } = useTranslation();
 
     const environment = getEnvironment();
-    const getBadgeColour = (env: Environments) => {
-        switch (env) {
-            case Environments.DEV:
-                return 'red';
-            case Environments.UAT:
-                return 'orange';
-            case Environments.PRD:
-                return 'green';
-        }
-    };
+
     return (
         <Flex pl={2} pr={2} pb={1} pt={1} align="center">
             <Heading as="h4" size="md" mr="auto">
-                {t('WindowHeader.heading')}
-                <Badge ml={1} colorScheme={getBadgeColour(environment)}>
-                    {environment === Environments.PRD ? getVersion() : environment}
-                </Badge>
+                {t('Seed Project')}
+                <RmgEnvBadge environment={environment} version={getVersion()} />
             </Heading>
         </Flex>
     );
