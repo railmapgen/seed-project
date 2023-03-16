ebfd106b7e49fb06a0ab970296c78af32b296ca6
Chito Wong <chi.to.wong@outlook.com>
2023-03-16 20:56:20 +0800

    Listen to colour mode change

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -3,7 +3,14 @@ import { Button } from '@chakra-ui/react';
 import WindowHeader from './window-header';
 import { useRootDispatch, useRootSelector } from '../redux';
 import { bumpCounter } from '../redux/app/app-slice';
-import { RmgDebouncedInput, RmgLabel, RmgPage, RmgPageHeader, RmgWindow } from '@railmapgen/rmg-components';
+import {
+    RmgDebouncedInput,
+    RmgLabel,
+    RmgPage,
+    RmgPageHeader,
+    RmgThemeProvider,
+    RmgWindow,
+} from '@railmapgen/rmg-components';
 import { useTranslation } from 'react-i18next';
 
 export default function AppRoot() {
@@ -12,29 +19,31 @@ export default function AppRoot() {
     const counter = useRootSelector(state => state.app.counter);
 
     return (
-        <RmgWindow>
-            <WindowHeader />
-            <RmgPage>
-                <RmgPageHeader>
-                    <RmgLabel label="Quick filter">
-                        <RmgDebouncedInput placeholder="Filter anything" />
-                    </RmgLabel>
-                </RmgPageHeader>
-                This is a seed project for RMG with React framework.
-                <br />
-                Please replace any &quot;RMG Seed Project&quot; or &quot;seed-project&quot; with the correct component
-                name.
-                <br />
-                Chakra UI and Redux store have been setup already. Here&apos;s an example state: {counter}.
-                <br />
-                <Button onClick={() => dispatch(bumpCounter())}>Bump</Button>
-                <br />
-                RMG Runtime has been setup. Click the button below to open RMG in another tab.
-                <br />
-                <Button onClick={() => rmgRuntime.openApp('rmg')}>
-                    {t('Open')} {t('Rail Map Generator')}
-                </Button>
-            </RmgPage>
-        </RmgWindow>
+        <RmgThemeProvider>
+            <RmgWindow>
+                <WindowHeader />
+                <RmgPage>
+                    <RmgPageHeader>
+                        <RmgLabel label="Quick filter">
+                            <RmgDebouncedInput placeholder="Filter anything" />
+                        </RmgLabel>
+                    </RmgPageHeader>
+                    This is a seed project for RMG with React framework.
+                    <br />
+                    Please replace any &quot;RMG Seed Project&quot; or &quot;seed-project&quot; with the correct
+                    component name.
+                    <br />
+                    Chakra UI and Redux store have been setup already. Here&apos;s an example state: {counter}.
+                    <br />
+                    <Button onClick={() => dispatch(bumpCounter())}>Bump</Button>
+                    <br />
+                    RMG Runtime has been setup. Click the button below to open RMG in another tab.
+                    <br />
+                    <Button onClick={() => rmgRuntime.openApp('rmg')}>
+                        {t('Open')} {t('Rail Map Generator')}
+                    </Button>
+                </RmgPage>
+            </RmgWindow>
+        </RmgThemeProvider>
     );
 }
