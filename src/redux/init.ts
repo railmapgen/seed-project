87da09a6b5e8ca6a0a9ccc40e33884272cc169a2
Chito Wong <chi.to.wong@outlook.com>
2024-05-18 18:24:41 +0800

    Use eslint flat config

--- src/redux/init.ts
+++ src/redux/init.ts
@@ -1,5 +1,6 @@
 import { RootStore } from './index';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function initStore(store: RootStore) {
     // startRootListening
 }
